810a95a8a956035a5ffc1e153260a55b07ac81ad
geoha <geohaxd@gmail.com>
2024-10-08 09:19:24 -0400

    Areglos a la logica de la interface: IDbReunionService

--- ReunionWeb/Services/IDbReunionService.cs
+++ ReunionWeb/Services/IDbReunionService.cs
@@ -1,5 +1,7 @@
-﻿using ReunionWeb.DTOs;
+﻿using NeoAPI.DTOs.ReunionDiaria;
+using ReunionWeb.DTOs;
 using ReunionWeb.NeoDbs;
+using ReunionWeb.ReunionDiaria.DTOs;
 
 namespace ReunionWeb.Services
 {
@@ -24,21 +26,21 @@ namespace ReunionWeb.Services
         //  Division centrodiscrepancia { get; set; }
 
         
-        Task GetPendientes(string centro, string div, DateTime f1, DateTime f2, string tipo, string estado);
-        Task <List<ReuDium>>GetByODT(string ODT, string idcentro, string iddiv);
-        Task<List<ReuDium>> GetHistoricos(string centro, string div, DateTime f1, DateTime f2, string tipo, string estado);
-        Task GetCambioStatus(int idreu);
-        Task GetCambioFecha(int idreu);
-        Task<ReuDium> GetDiscrepantacia(int id);
-        Task<CentroDivision> GetCentroDiv(string centro, string division, int tipo);
+        Task<List<ReuDiumDTO>> GetPendientes(string idcentro, string iddiv, DateTime f1, DateTime f2, string tipo, string estado);
+        Task<List<ReuDiumDTO>> GetByODT(string ODT, string idcentro, string iddiv);
+        Task<List<ReuDiumDTO>> GetHistoricos(string idcentro, string iddiv, DateTime f1, DateTime f2, string tipo, string estado);
+        Task<List<CambStatDTO>> GetCambioStatus(int idreu);
+        Task<List<CambFecDTO>> GetCambioFecha(int idreu);
+        Task<List<ReuDiumDTO>> GetDiscrepantacia(int id);
+        Task<List<CentroDivisionDTO>> GetCentroDiv(string centro, string division, int tipo);
 
 
 
         Task<int> InsertDiscrepancia(ReuDium discre);
-        Task InsertCambioStatus(CambStat status);//Insertar usuario que cambio el status
-        Task InsertCambioFec(CambFec cambiofec);//Insertar el usuarrio que cambio la fecha de trabajo.
-        Task<bool> InsertarRegistros(CambFec cambiofec, CambStat status);//Insertar discrepancias con chismoso(Trazabilidad)
-        Task<bool> UpdateDiscrepancia(ReuDium bdDiv1, int id, int tipo, string f1, string f2, string estdo);
+        Task<bool> InsertCambioStatus(CambStat status);//Insertar usuario que cambio el status
+        Task<bool> InsertCambioFec(CambFec cambiofec);//Insertar el usuarrio que cambio la fecha de trabajo.
+        Task<bool> InsertarRegistros(RegistroCambiosDTO registroCambios);//Insertar discrepancias con chismoso(Trazabilidad)
+        Task<bool> UpdateDiscrepancia(ReuDium d, int id, int tipo, string f1, string f2, string estado);
         Task<bool> UpdateDiscrepancia2(ReuDium bdDiv1, int id, int tipo, string f1, string f2, string estdo, string linea);
 
 
