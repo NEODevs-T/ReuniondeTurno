53c5a9f49979998d50e70802e9084d60c2c4ee42
geoha <geohaxd@gmail.com>
2024-11-08 14:55:42 -0400

    Pruebas de nuevas implementaciones

--- ReunionWeb/DTOs/ReunionDiaria/RegistroCambiosDTO.cs
+++ ReunionWeb/DTOs/ReunionDiaria/RegistroCambiosDTO.cs
@@ -4,11 +4,14 @@ namespace ReunionWeb.ReunionDiaria.DTOs;
 
 public class RegistroCambiosDTO
 {
-    public RegistroCambiosDTO(CambFecDTO cambFecDTO, CambStatDTO cambStatDTO)
+    public RegistroCambiosDTO(CambFecDTO cambFecDTO, CambStatDTO cambStatDTO, ReuDiumDTO regisReudia)
     {
         this.cambFecDTO = cambFecDTO;
         this.cambStatDTO = cambStatDTO;
+        this.regisReudia = regisReudia;
     }
     public CambFecDTO? cambFecDTO { get; set; }
     public CambStatDTO? cambStatDTO { get; set; }
+    public ReuDiumDTO? regisReudia { get; set; }
+
 }
