add5aaddf01c6730e6ed5fb41e5d50d4297f9823
Jesus Esquivel <jesus.esquivel@papeleslatinos.com>
2025-09-10 15:58:29 -0400

    Prueba de traduccion

--- ReunionWeb/Program.cs
+++ ReunionWeb/Program.cs
@@ -4,22 +4,24 @@ using ReunionWeb.Interface;
 using BlazorStrap;
 using Microsoft.EntityFrameworkCore;
 using ReunionWeb.Services;
-
 using ReunionWeb;
 using Radzen;
 using ReunionWeb.Data;
-
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
-builder.Services.AddHttpClient();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
 
+// Servicios de tu aplicación
 builder.Services.AddHttpClient<IAPIReunionService, APIReunionService>(client =>
 {
     client.BaseAddress = new Uri("http://neo.paveca.com.ve/ReunionApi/");
 });
+
 builder.Services.AddScoped<IDbReunionService, DbReunionService>();
 builder.Services.AddScoped<IAPIReunionService, APIReunionService>();
 builder.Services.AddScoped<IMaestraData, MaestraData>();
@@ -32,11 +34,18 @@ builder.Services.AddScoped<IRespoReuData, RespoReuData>();
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<ICausaCalidadData, CausaCalidadData>();
-builder.Services.AddControllersWithViews();
-builder.Services.AddBlazorStrap();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<UserCultureService>();
+
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazorStrap();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+// ✅ Localización
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddControllersWithViews()
+       .AddDataAnnotationsLocalization(); // importante para validaciones
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -44,16 +53,28 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UsePathBase("/reuniondeturnoDESARROLLO");
 app.UseStaticFiles();
+
+// Configuración de culturas
+var supportedCultures = new[] { "es-ES", "en-US" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture("es-ES")
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+// Redirección inicial
 app.MapGet("/", context =>
 {
     context.Response.Redirect("/reuniondeturnoDESARROLLO/es/inicio");
     return Task.CompletedTask;
 });
+
 app.Run();
