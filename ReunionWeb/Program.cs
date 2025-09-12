dfa13c044db624605b14ac34887355ff6003703d
Humberto7913 <humbertoalfonso8@gmail.com>
2025-09-12 10:20:29 -0400

    Se realizaron unos cambios para probar si la traduccion no se pierde al momento que hayan dos o mas usuarios dentro de la reunion

--- ReunionWeb/Program.cs
+++ ReunionWeb/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddHttpClient<IAPIReunionService, APIReunionService>(client =>
 {
     client.BaseAddress = new Uri("http://neo.paveca.com.ve/ReunionApi/");
 });
+builder.Services.AddScoped<CultureService>();
 builder.Services.AddScoped<IDbReunionService, DbReunionService>();
 builder.Services.AddScoped<IAPIReunionService, APIReunionService>();
 builder.Services.AddScoped<IMaestraData, MaestraData>();
