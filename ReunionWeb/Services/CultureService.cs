51cbfc0d861f6d750130af2a6e632399b311380b
Humberto7913 <humbertoalfonso8@gmail.com>
2025-09-17 16:15:38 -0400

    Se realizaron algunas modificaciones para las traducciones segun observaciones

--- ReunionWeb/Services/CultureService.cs
+++ ReunionWeb/Services/CultureService.cs
@@ -1,22 +1,41 @@
 using System.Globalization;
+using ReunionWeb.Services;
 
 namespace ReunionWeb.Services
 {
     public class CultureService
     {
+        private readonly UsuarioContexto _usuarioContexto;
+
         public CultureInfo UserCulture { get; private set; } = new CultureInfo("es");
+
+        public CultureService(UsuarioContexto usuarioContexto)
+        {
+            _usuarioContexto = usuarioContexto;
+        }
 
         public void SetCulture(string cultureName)
         {
             if (!string.IsNullOrWhiteSpace(cultureName))
             {
                 UserCulture = new CultureInfo(cultureName);
+                CultureInfo.DefaultThreadCurrentCulture = UserCulture;
+                CultureInfo.DefaultThreadCurrentUICulture = UserCulture;
             }
         }
 
         public string GetCultureName()
         {
             return UserCulture.Name;
+        }
+
+        public async Task EstablecerCulturaDesdeUsuarioAsync()
+        {
+            var usuario = await _usuarioContexto.ObtenerUsuarioActualAsync();
+            if (usuario != null)
+            {
+                SetCulture(usuario.CultureName);
+            }
         }
     }
 }
