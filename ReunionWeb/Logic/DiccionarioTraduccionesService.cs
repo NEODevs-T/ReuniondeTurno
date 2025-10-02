a8fa1519d60e1f842b4d5152ffa020b28804bfac
Humberto7913 <humbertoalfonso8@gmail.com>
2025-10-02 15:46:05 -0400

    Se acomodo la traduccion de la vista DiasNoLaborablesDialog

--- ReunionWeb/Logic/DiccionarioTraduccionesService.cs
+++ ReunionWeb/Logic/DiccionarioTraduccionesService.cs
@@ -15,18 +15,17 @@ namespace ReunionWeb.Services
         {
             var claves = new[]
             {
-            "Atencion", "Asistencia", "Asistentes", "Bloque", "BuscarCargo", "BuscarCargo2", "Cargo", "Centro", "Consultar","DiasNoLaborables", "Empresa",
+            "Atencion", "Asistencia", "Asistentes", "Bloque", "BuscarCargo", "BuscarCargo2", "Cancelar", "Cargo", "Centro", "Consultar","DiasNoLaborables", "Empresa",
             "ExcluirLosDiasNoLaborables", "FechaFinal", "FechaInicial", "FiltrarPorCargo", "Guardar",
             "LimpiarFiltros", "NoCargadoP", "NoExisteEseCargo",  "NoHayFiltros", "Personal", "PorcentajeAsistencia",
-            "PorcentajeDeAsistencia", "PorcentajeGlobal", "Presencia", "ReunionesAsistidas",
-            "ReunionesProgramadas", "SeleccioneUnCentro", "SeleccioneUnaEmpresa", "SSuplente", "Suplencia", "TCargos", "RExitoso", "RHorario", "EDato"
+            "PorcentajeDeAsistencia", "PorcentajeGlobal", "Presencia", "ReunionesAsistidas", "AgregarFecha", "FechasSeleccionadas",
+            "ReunionesProgramadas", "SeleccioneUnCentro", "SeleccioneUnaEmpresa", "SeleccioneDML", "SSuplente", "Suplencia", "TCargos", "RExitoso", "RHorario", "EDato"
         };
 
             foreach (var clave in claves)
             {
                 var traduccion = _translator.Traducir(clave);
                 Traducciones[clave] = traduccion;
-                Console.WriteLine($"Clave: {clave} => {(string.IsNullOrEmpty(traduccion) ? "SIN TRADUCCIÓN" : traduccion)}");
             }
 
             await Task.CompletedTask;
