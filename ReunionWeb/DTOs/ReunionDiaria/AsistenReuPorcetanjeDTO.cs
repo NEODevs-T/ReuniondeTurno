842c30648b5e861c0eecbc5105a08ab6df3e7178
Humberto7913 <humbertoalfonso8@gmail.com>
2025-11-14 14:51:13 -0400

    Se modifico la tabla del porcentaje asistencia para que muestre la suplencia en las reuniones

--- ReunionWeb/DTOs/ReunionDiaria/AsistenReuPorcetanjeDTO.cs
+++ ReunionWeb/DTOs/ReunionDiaria/AsistenReuPorcetanjeDTO.cs
@@ -11,10 +11,13 @@ public partial class AsistenReuPorcetanjeDTO
     public int ReunionesProgramadas { get; set; }
     public int ReunionesAsistidas { get; set; }
     public double PorcentajeAsistencia { get; set; }
+    public int ReunionesSuplencias { get; set; }
+    public double PorcentajeSuplencia { get; set;}
 }
 
 public class PorcentajeAsistenciaTurnoResponseDTO
 {
     public double PorcentajeGlobal { get; set; }
+    public double PorcentajeGlobalSuplencia { get; set; }
     public List<AsistenReuPorcetanjeDTO> DetallePorCargo { get; set; }
 }
